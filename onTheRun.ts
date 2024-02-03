4d12b688c0537dbfa1caa6031b2b384227a843ca
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-02-03 18:31:40 -0500

    Added Dice Rolling function

--- onTheRun.ts
+++ onTheRun.ts
@@ -1,1 +1,7 @@
 /* Add code here */
+
+/* This is our Dice Rolling function */
+function randomNum(min: number, max: number) {
+    let range: number = max - min + 1;
+    return Math.floor(Math.random() * range) + min;
+  }

a337044891c54364ab91cf58754092c5196c4a3a
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-02-06 16:54:07 -0500

    Typescript work

--- onTheRun.ts
+++ onTheRun.ts
@@ -1,5 +1,7 @@
 /* Add code here */
 
+/* I need to build out a table for the rolls */
+
 /* This is our Dice Rolling function */
 function randomNum(min: number, max: number) {
     let range: number = max - min + 1;

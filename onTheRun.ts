bceed33ef6a13e3a034a12f0f96c0afcf7ec894d
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-07-17 21:01:39 -0400

    Notes

--- onTheRun.ts
+++ onTheRun.ts
@@ -1,6 +1,7 @@
 /* Add code here */
 
 /* I need to build out a table for the rolls */
+/* Need to build out a fun front end */
 
 /* This is our Dice Rolling function */
 function randomNum(min: number, max: number) {
